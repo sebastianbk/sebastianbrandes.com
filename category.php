b83b7f812378be20aab7abbb2db65e9a42c3afef
Sebastian Brandes Kraaijenzank <sb@sebastianbrandes.com>
2015-09-08 10:47:43 +0200

    Removed abundant h1 tag from category.php

--- category.php
+++ category.php
@@ -21,9 +21,7 @@
 	<main>
 		<!-- section -->
 		<section>
-
-			<h1><?php _e( 'Categories for ', 'sebastianbrandes' ); single_cat_title(); ?></h1>
-
+			
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
